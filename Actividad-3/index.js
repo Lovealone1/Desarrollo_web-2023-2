19cec63df0051e7b6cc948abf1709a24b3cef841
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-08-15 18:06:36 -0500

    feat: Actividad 3 JavaScript

---
+++ Actividad-3/index.js
@@ -0,0 +1,15 @@
+console.log("Borojo")
+
+const edad = 55;
+let nombre = "Carmelo";
+const estudiante = false;
+const altura = 1.90;
+const semestres = [];
+const materiaP = {nombre: "Calculo", id: 0};
+const universidad = null; 
+const carrera = undefined;
+
+console.log(typeof nombre);
+const precioMatricula = "50000000.5";
+console.log(parseInt(precioMatricula))
+console.log(parseFloat(precioMatricula))
